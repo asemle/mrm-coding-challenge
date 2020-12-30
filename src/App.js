ed8ed524d8660553b75c75e287b99a45c1e8a8a5
asemle <asemle@umass.edu>
2020-12-30 17:14:41 -0500

    deploy to gh pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      'filter': [],
+      'list': ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit", "Odio", "tempore", "Voluptates", "quidem", "rem", "vel", "labore", "dolores", "nemo", "possimus", "Recusandae", "eaque"]
+    }
+  }
+  updateFilter = (value) => {
+    if(value === "reset") {
+      this.setState({
+        'filter': []
+      })
+    }
+    else if(this.state.filter.indexOf(value) === -1) {
+      this.setState({
+        'filter': [...this.state.filter, value]
+      })
+    } else {
+      var newArr = [...this.state.filter];
+      newArr.splice(newArr.indexOf(value), 1);
+      this.setState({
+        'filter': newArr
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="buttonBox">
+          <button className={this.state.filter.indexOf("a") > -1 ? 'selected' : ''} value="a" onClick={e => this.updateFilter(e.target.value)}>a</button>
+          <button className={this.state.filter.indexOf("e") > -1 ? 'selected' : ''}  value="e" onClick={e => this.updateFilter(e.target.value)}>e</button>
+          <button className={this.state.filter.indexOf("i") > -1 ? 'selected' : ''}  value="i" onClick={e => this.updateFilter(e.target.value)}>i</button>
+          <button value="reset" onClick={e => this.updateFilter(e.target.value)}>reset</button>
+        </div>
+        <ol>
+          {this.state.list
+            .filter(word => !this.state.filter.some(el => word.includes(el)))
+            .map((element, i) =>
+              <li key={i}>{element}</li>
+            )
+          }
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
+
